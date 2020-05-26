7bdcf7f32e2ec8b783bf4c144517cd371700cd4a
Martin-Bozon <62134476+Martin-Bozon@users.noreply.github.com>
2020-05-26 16:51:25 +0200

    récupération date au bon format

--- include/php_commentaire.php
+++ include/php_commentaire.php
@@ -7,7 +7,7 @@ if(isset($_SESSION["login"]))
                 $commentaire = $_POST["com"];
                 $id_user = $_SESSION["id"];
                 $connexionbd = mysqli_connect("localhost" , "root" , "" , "livreor");
-                $requete_com = "INSERT INTO commentaires (commentaire, id_utilisateur, date) VALUES ('$commentaire', $id_user, NOW())";
+                $requete_com = "INSERT INTO commentaires (commentaire, id_utilisateur, date) VALUES ('$commentaire', $id_user, CUREDATE(DATE_FORMAT(date, '%d/%m/%Y')))";
                 $send_com = mysqli_query($connexionbd , $requete_com);
                 mysqli_close($connexionbd);
                 header("Location:livre-or.php");
--- include/php_livre-or.php
+++ include/php_livre-or.php
@@ -1,8 +1,9 @@
 <?php
 
         $connexionbd = mysqli_connect("localhost" , "root" , "" , "livreor");
-        $requete = "SELECT commentaires.commentaire, commentaires.date, utilisateurs.login FROM commentaires INNER JOIN utilisateurs ON commentaires.id_utilisateur = utilisateurs.id";
+        $requete = "SELECT commentaires.commentaire, DATE_FORMAT(date, '%d/%m/%Y') AS date, utilisateurs.login FROM commentaires INNER JOIN utilisateurs ON commentaires.id_utilisateur = utilisateurs.id";
         $send_requete = mysqli_query($connexionbd, $requete);
         $commentaire_info = mysqli_fetch_all($send_requete, MYSQLI_ASSOC);
         var_dump($commentaire_info); 
+
 ?>
